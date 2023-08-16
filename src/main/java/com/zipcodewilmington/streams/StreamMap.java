ac57ac54e5397f87ce2be8328963a7077950235e
Joe <Jhuntenburg@gmail.com>
2023-08-15 20:03:04 -0400

    some done

--- src/main/java/com/zipcodewilmington/streams/StreamMap.java
+++ src/main/java/com/zipcodewilmington/streams/StreamMap.java
@@ -18,7 +18,9 @@ public class StreamMap {
      * @return - a Stream of single characters
      */ //TODO
     public static Stream<String> letters(String someWord) {
-        return null;
+
+        return someWord.chars()
+                .mapToObj(c -> String.valueOf((char) c));
     }
 
     /**
@@ -26,7 +28,9 @@ public class StreamMap {
      * @return - a Stream of several Streams of single characters
      */ //TODO
     public static Stream<Stream<String>> wordsMap(String... someWords) {
-        return null;
+
+        return Stream.of(someWords)
+                .map(word -> word.chars().mapToObj(c -> String.valueOf((char) c)));
     }
 
     /**
@@ -34,6 +38,8 @@ public class StreamMap {
      * @return - a Stream of several Streams of single characters
      */ //TODO
     public static Stream<String> wordsFlatMap(String... stringArray) {
-        return null;
+
+        return Stream.of(stringArray)
+                .flatMap(word -> word.chars().mapToObj(c -> String.valueOf((char) c)));
     }
 }
